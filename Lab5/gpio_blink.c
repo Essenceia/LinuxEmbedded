7d47df82b67683061b4ce5fe410713368ec91e2f
pookie <pookie@localhost.localdomain>
2017-11-08 11:07:23 +0100

    statted gpio blink

--- Lab5/gpio_blink.c
+++ Lab5/gpio_blink.c
@@ -1,4 +1,59 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/gpio.h>
+#include <linux/init.h>
 
+MODULE_AUTHOR("Julia Desmazes");
+MODULE_LICENSE("GPLv3");
+MODULE_DESCRIPTION("Blink LED modules");
+
+//timer
+struct timer_list my_timer;
+struct tty_driver *my_driver;
+char kbledstatus = 0;
+#define BLINK_DELAY HZ/5
+#define LED_ON 0x01
+#define LED_OFF 0xFF
+//gpio defined
+#define GPIO_PIN_NUMB 0X1A
+
+static void my_timer_func(unsigned long ptr)
+{
+    printk(KERN_INFO"Blink led");
+    int *pstatus = (int *)ptr;
+    *pstatus= ((*pstatus == LED_ON)? LED_OFF : LED_ON);
+    gpio_set_value(GPIO_PIN_NUMB,*pstatus);
+    my_timer.expires = jiffies + BLINK_DELAY;
+    add_timer(&my_timer);
+}
+
+static int __init start_blink(void)
+{
+    int err;
+printk(KERN_INFO"Loading blink led module, blinking shall start at a rate of 1 second.\n");
+    //init gpio
+    err = gpio_direction_output(GPIO_PIN_NUMB, LED_OFF);
+    if(err < 0){
+        printk(KERN_ERR)
+    }
+//init timer
+init_timer(&my_timer);
+my_timer.function = my_timer_func;
+my_timer.data = (unsigned long)&kbledstatus;
+my_timer.expires = jiffies + BLINK_DELAY;
+add_timer(&my_timer);
+return 0;
+}
+
+static void __exit stop_blink(void)
+{
+printk(KERN_INFO "Blink module is beeing unloaded , blinking will stop\n");
+//delet timer
+del_timer(&my_timer);
+//turn off led
+
+gpio_set_value(GPIO_PIN_NUMB,LED_OFF);
+
+}
+
+module_init(start_blink);
+module_exit(stop_blink);
